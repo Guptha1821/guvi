9893c6dda2b56047783369de6b68ea386614cb24
Guptha1821 <38830297+Guptha1821@users.noreply.github.com>
2019-08-06 21:47:54 +0530

    Add files via upload

---
+++ greatest_number_among_three_values.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  6 21:36:48 2019
+
+@author: guptha
+"""
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+c=int(input("enter the value:"))
+l=[]
+l.append(a)
+l.append(b)
+l.append(c)
+print(max(l))
